0be1962d682a6731655fd5ef1708c4e1597dc3a5
Your Name <felix.acevedo.tech@gmail.com>
2022-02-24 21:30:29 -0500

    drag drop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,75 @@ $(function(){
     localStorage.setItem("tasks", JSON.stringify(tasks));
   };
 
+  // enable draggable/sortable feature on list-group elements
+  $(".card .list-group").sortable({
+    // enable dragging across lists
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event, ui) {
+      console.log(ui);
+    },
+    deactivate: function(event, ui) {
+      console.log(ui);
+    },
+    over: function(event) {
+      console.log(event);
+    },
+    out: function(event) {
+      console.log(event);
+    },
+    update: function() {
+      var tempArr = [];
 
+      // loop over current set of children in sortable list
+      $(this)
+        .children()
+        .each(function() {
+          // save values in temp array
+          tempArr.push({
+            text: $(this)
+              .find("p")
+              .text()
+              .trim(),
+            date: $(this)
+              .find("span")
+              .text()
+              .trim()
+          });
+        });
+
+      // trim down list's ID to match object property
+      var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+    },
+    stop: function(event) {
+      $(this).removeClass("dropover");
+    }
+  });
+
+  // trash icon can be dropped onto
+  $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      // remove dragged element from the dom
+      ui.draggable.remove();
+
+    },
+    over: function(event, ui) {
+      console.log(ui);
+    },
+    out: function(event, ui) {
+      console.log(ui);
+    }
+  });
 
 
   // modal was triggered
